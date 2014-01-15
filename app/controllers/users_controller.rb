a282baa830be10372d39d838d2256f341b02a507
princesspretzel <brittanga@gmail.com>
2014-01-15 17:06:44 -0500

    more views, routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,34 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @usermuserovie = User.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+      @user.update_attributes(params[:user])
+
+    if @user.update_attributes(params[:user])
+      redirect_to @user, notice: 'user was successfully updated.' 
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+
+    redirect_to users_path
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,17 @@ Fishbowl::Application.routes.draw do
 
   devise_for :users
 
- root :to => "home#index"
+  root :to => "home#index"
+
+  resources :users
+
+  resources :activities
+
+  get '/users' => "users#index"
+  get '/users/:id' => "users#show"
+
+  get '/activities' => "activities#index"
+  get '/activities/:id' => "activities#show"
+  post '/activities' => "activities#create"
 
 end
